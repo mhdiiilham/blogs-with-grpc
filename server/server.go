6b509442a2b42f3c0e7fc197167486db250a8625
Muhammad Ilham <muhd.iiilham@gmail.com>
2020-11-14 20:58:34 +0700

    fix(server/server.go): fix Server Logging

--- server/server.go
+++ server/server.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"os"
+	"os/signal"
 
 	blogpb "blogs/server/protos"
 
@@ -13,6 +15,10 @@ import (
 type server struct{}
 
 func main() {
+	// if we crash the go code,
+	// we get the file and line number
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+
 	fmt.Println("Blog server")
 
 	lis, err := net.Listen("tcp", "localhost:50051")
@@ -24,7 +30,19 @@ func main() {
 	s := grpc.NewServer()
 	blogpb.RegisterBlogServiceServer(s, &server{})
 
-	if err := s.Serve(lis); err != nil {
-		log.Fatalf("Failed to server: %v", err)
-	}
+	go func() {
+		fmt.Println("Starting server")
+		if err := s.Serve(lis); err != nil {
+			log.Fatalf("Failed to server: %v", err)
+		}
+	}()
+
+	ch := make(chan os.Signal, 1)
+	signal.Notify(ch, os.Interrupt)
+
+	<-ch
+	fmt.Println("Stopping the server")
+	s.Stop()
+	fmt.Println("Closing listener")
+	lis.Close()
 }
