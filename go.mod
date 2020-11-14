3d1c9d118de2fcf733227684465ade33567a2b62
Muhammad Ilham <muhd.iiilham@gmail.com>
2020-11-14 21:35:59 +0700

    refactor(server/server.go): refactoring Server Main Function

--- go.mod
+++ go.mod
@@ -4,5 +4,7 @@ go 1.15
 
 require (
 	github.com/golang/protobuf v1.4.1
+	github.com/joho/godotenv v1.3.0
+	go.mongodb.org/mongo-driver v1.4.3
 	google.golang.org/grpc v1.33.2
 )
--- server/server.go
+++ server/server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net"
@@ -9,40 +10,72 @@ import (
 
 	blogpb "blogs/server/protos"
 
+	_ "github.com/joho/godotenv/autoload"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 	"google.golang.org/grpc"
 )
 
-type server struct{}
+type server struct {
+	Network    string
+	Address    string
+	Collection *mongo.Collection
+}
 
 func main() {
 	// if we crash the go code,
 	// we get the file and line number
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
 
-	fmt.Println("Blog server")
+	// Connection to MongoDB
+	collection := mongoDBConnection()
 
-	lis, err := net.Listen("tcp", "localhost:50051")
+	// Blogs server instance
+	blogServer := newServer(collection)
 
+	lis, err := net.Listen(blogServer.Network, blogServer.Address)
 	if err != nil {
 		log.Fatalf("Failed to listen to port 50051. Error: %v", err)
 	}
-
 	s := grpc.NewServer()
-	blogpb.RegisterBlogServiceServer(s, &server{})
-
+	blogpb.RegisterBlogServiceServer(s, &blogServer)
 	go func() {
 		fmt.Println("Starting server")
 		if err := s.Serve(lis); err != nil {
 			log.Fatalf("Failed to server: %v", err)
 		}
 	}()
-
 	ch := make(chan os.Signal, 1)
 	signal.Notify(ch, os.Interrupt)
-
 	<-ch
 	fmt.Println("Stopping the server")
 	s.Stop()
 	fmt.Println("Closing listener")
 	lis.Close()
+}
+
+func newServer(mongoDBCollection *mongo.Collection) *server {
+	return &server{
+		Network:    os.Getenv("SERVER_NETWORK"),
+		Address:    os.Getenv("SERVER_ADDRESS"),
+		Collection: mongoDBCollection,
+	}
+}
+
+func mongoDBConnection() *mongo.Collection {
+	mongoURI := fmt.Sprintf(
+		"mongodb+srv://%s:%s@cluster0.ub9ns.mongodb.net/%s?retryWrites=true&w=majority",
+		os.Getenv("MONGO_DB_USER"),
+		os.Getenv("MONGO_DB_PASS"),
+		os.Getenv("MONGO_DB_DB"),
+	)
+	client, err := mongo.NewClient(options.Client().ApplyURI(mongoURI))
+	if err != nil {
+		log.Fatalf("Cannot connect to MongoDB: %v", err)
+	}
+	if err := client.Connect(context.TODO()); err != nil {
+		log.Fatal(err)
+	}
+	collection := client.Database(os.Getenv("MONGO_DB")).Collection(os.Getenv("MONGO_DB_COLLECTION"))
+	return collection
 }
